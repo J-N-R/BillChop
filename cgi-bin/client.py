238028b0a45c71f882b7b623bdfa07608a50bf22
J-N-R <rivejona@kean.edu>
2021-11-01 14:25:13 -0400

    Setup base python files

---
+++ cgi-bin/client.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3.8
+
+#
+# client.py
+#
+# Description:
+#    Client program for BillChop.
+#    Will accept data from a HTML form and send to a running python server.
+#
+#    If a server cannot be found, create one.
+#
+# Made By:
+#   whoever@kean.edu
+#
+
+# import necessary stuff
+
+# maybe a couple of if statments here
+
+# send information with imported function
---
+++ cgi-bin/server.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3.8
+
+#
+# server.py
+#
+# Description:
+#    Server Program for BillChop.
+#    Acts as a server that people's data can connect to.
+#    
+#    Each user will send what they want to buy here; eggs $3, drink $4, etc
+#    and the server will sum it up and print out final results once everyone has set their data.
+#
+#    All results will be printed in JSON, including errors
+#
+# Made By:
+#   whoever@kean.edu
+#
+
+
+# import stuff for server, if needed we can install something
+
+# accept incoming connections (there will need to be a client.py that accepts cgi and connects to server)
+
+# be able to sum up a client's chosen options
