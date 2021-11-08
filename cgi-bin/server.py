358c6962f87bce812020da71126656d1f9f4f3b6
J-N-R <rivejona@kean.edu>
2021-11-08 13:45:57 -0500

    add server timeout, comment code

--- cgi-bin/server.py
+++ cgi-bin/server.py
@@ -22,20 +22,43 @@ import socket
 
 print("Server Starting...")
 
+# Setup Connection
 s = socket.socket()
+
+# On Kean's IP, use port 7500
 s.bind(("131.125.80.107", 7500))
+
+# Kill the server if no connections after 2 minutes
+s.settimeout(120)
+
+# Max 5 connections at once
 s.listen(5)
 
 count = 0
 
-while count < 2:
-    conn, address = s.accept()
-    count = count + 1
-    print(conn.recv(1024).decode())
-    conn.send('Hi! -Server'.encode())
-    conn.close()
+try:
+
+  # While we've only accepted less than two connections,
+    while count < 2:
+
+      # Wait for new connection (the code after only runs after successful connection)
+        conn, address = s.accept()
+
+      # Connection success, add 1 to counter, and send a message
+        count = count + 1
+        print(conn.recv(1024).decode())
+        conn.send('Hi! -Server'.encode())
+        conn.close()
+
+except socket.timeout:
+    print("No connections found.")
+
+except:
+    print("Error connecting!")
 
 print("Connection Finished")
-# accept incoming connections (there will need to be a client.py that accepts cgi and connects to server)
+
+# TODOS
+# DONE: accept incoming connections (there will need to be a client.py that accepts cgi and connects to server)
 
 # be able to sum up a client's chosen options
