263dfebc2a0ec7edca79b467ec7d8c89f4cafcbd
J-N-R <rivejona@kean.edu>
2021-11-09 00:16:33 -0500

    Comment code, add error handling

--- cgi-bin/client.py
+++ cgi-bin/client.py
@@ -7,15 +7,9 @@
 #    Client program for BillChop.
 #    Will accept data from a HTML form and send to a running python server.
 #
-#    If a server cannot be found, create one.
-#
 # Made By:
-#   whoever@kean.edu
+#    rivejona@kean.edu
 #
-
-# import necessary stuff
-
-# maybe a couple of if statments here
 
 # send information with imported function
 
@@ -24,9 +18,18 @@ import easySocket
 print("Client Starting...")
 
 try:
-    conn = easySocket.connect_tcp("131.125.80.107", 7500)
+
+  # Attempt to connect to server (code after only runs after success)
+    conn = easySocket.connect_tcp("localhost", 7500)
+
+  # Send sample data
     conn.send('Hi! -client'.encode())
     print(conn.recv(1024).decode())
-    print("Connection finished!")
+
+except ConnectionRefusedError:
+    print("No Server Detected!")
+
 except:
-    print("Error Connecting! Please try again. Error: ")
+    print("Error Connecting! Please try again.")
+
+print("Connection Finished!")
--- cgi-bin/server.py
+++ cgi-bin/server.py
@@ -26,7 +26,7 @@ print("Server Starting...")
 s = socket.socket()
 
 # On Kean's IP, use port 7500
-s.bind(("131.125.80.107", 7500))
+s.bind(("localhost", 7500))
 
 # Kill the server if no connections after 2 minutes
 s.settimeout(120)
@@ -34,24 +34,28 @@ s.settimeout(120)
 # Max 5 connections at once
 s.listen(5)
 
-count = 0
+billCount = 0
 
 try:
 
   # While we've only accepted less than two connections,
-    while count < 2:
+    while billCount < 2:
 
-      # Wait for new connection (the code after only runs after successful connection)
+      # Attempt to connect to client (code after only runs after success)
         conn, address = s.accept()
 
-      # Connection success, add 1 to counter, and send a message
-        count = count + 1
+      # Send sample data, increase billCount
+        billCount = billCount + 1
         print(conn.recv(1024).decode())
         conn.send('Hi! -Server'.encode())
         conn.close()
 
 except socket.timeout:
     print("No connections found.")
+
+except KeyboardInterrupt:
+    s.close()
+    print("Server force closed")
 
 except:
     print("Error connecting!")
